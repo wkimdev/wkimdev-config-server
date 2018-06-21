62aad679ef9c03db320a0a605eb1d8df998c7300
wkim <wkimdev@gmail.com>
2018-06-21 19:01:02 +0900

    Task : first config server

---
+++ wkimdev-config-server/src/test/java/com/home/wkim/WkimdevConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.home.wkim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WkimdevConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
